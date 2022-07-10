be7fcf1b9accccf53617b77727c8a1f2cf038fef
Kevin Zhou <kzhou13@forhdam.edu>
2022-07-10 19:56:50 -0400

    this is the solution

--- Lesson_2.py
+++ Lesson_2.py
@@ -7,3 +7,20 @@
 
 print("Problem received")
 
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], sum: int) -> bool:
+        if not root:
+            return False
+
+        if not root.left and not root.right and root.val == sum:
+            return True
+        
+        sum -= root.val
+
+        return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
+
+
+
+
+
