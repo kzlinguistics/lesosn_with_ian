d4833cde1c88fddb7032cd0f67ab2eac016e841b
Kevin Zhou <kzhou13@forhdam.edu>
2022-07-10 20:58:09 -0400

    check the question that I self answered

--- Lesson_2.py
+++ Lesson_2.py
@@ -12,26 +12,36 @@ print("Problem received")
 
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], sum: int) -> bool:
+
+        #we want to call our function recursively
+        #hence the last line tbh
         
         #if not root. you need to get false because you need to stop at root. 
         if not root:
             return False
 
+        ########
+        #question 1- how do I know this is going to work and keep updating. there is not 
+        #for loop here. Is it being driven by the last line? 
 
-        sum -= root.val
+        #answer from myself- yes 
+
+
+        ########
+        sum -= root.val #we just keep updating the sum 
 
         #this is to look for the leaf 
         #this is an "iterative" solution. I need to stop thinking about the first pass 
 
-
+        #this is to check if the root has a root to leaf path that leads to 0 
+        
         if not root.left and not root.right:
-            return sum == 0
+
+            #once we hit our leaf node, we want to see if our "sum" is 0 
+            return sum == 0 
         
         #the tree you are searching for need to have a value that is 
         #smaller than the one you start the root with. 
-
-        #remember the example Youtube, If the goal is 22, the root is 5. The "sum"
-        #should be 17 
 
         #if it does not equal. you have to keep on going. the magic of CS 
         return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
